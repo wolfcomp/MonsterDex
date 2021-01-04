041176577f083e2b33a7f0d25eb6f0622f082794
Lyra Reyson <59393535+Strati@users.noreply.github.com>
2021-01-04 08:53:29 -0600

    update

---
+++ TargetData (2).cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Dalamud.Plugin;
+using Dalamud.Game.ClientState.Actors.Types;
+using Dalamud.Game.ClientState.Actors.Types.NonPlayer;
+
+namespace DeepDungeonDex
+{
+	public class TargetData
+	{
+		class MobData
+		{
+			public enum threatLevel;
+			public enum aggroType;
+			public bool isStunnable;
+			public string mobNotes;
+
+			enum threatLevel
+            {
+				Unspecified,
+				Easy,
+				Caution,
+				Danger,
+				DoNotEngage
+            }
+			
+
+
+		}
+
+		public static void Mobs()
+        {
+			var mobs = new Dictionary<int, Mobdata>()
+            {
+				{7262, new MobData { threatLevel=1, aggroType=1, isStunnable=true, mobNotes="" } }
+            }
+        }
+	}
+}
