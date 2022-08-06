c2ae137a5f191b16e092a9c633c1a4b5a2cd669e
WildWolf <magnus1997.mho@gmail.com>
2022-08-06 19:57:19 +0200

    chore: final fix

--- scripts/gitPushPr.sh
+++ scripts/gitPushPr.sh
@@ -1,21 +1,21 @@
 cd repo
 echo "> Adding and committing"
 git add --all
-git commit --all -m "Updating $1"
+git commit --all -m "Updating ${PUBLIC_NAME}"
 echo "> Pushing to origin"
-git push --force --set-upstream origin "$1"
+git push --force --set-upstream origin "${PUBLIC_NAME}"
 prRepo="goatcorp/DalamudPluginsD17"
-prNumber=$(gh api repos/${prRepo}/pulls | jq ".[] | select(.head.ref == \"$2\") | .number")
-if [[ $4 =~ .*"[TEST]".* ]]; then
-    prTitle="[Testing] $1 $3"
+prNumber=$(gh api repos/${prRepo}/pulls | jq ".[] | select(.head.ref == \"${INTERNAL_NAME}\") | .number")
+if [[ ${MESSAGE} =~ .*"[TEST]".* ]]; then
+    prTitle="[Testing] ${PUBLIC_NAME} ${VERSION}"
 else
-    prTitle="$1 $3"
+    prTitle="${PUBLIC_NAME} ${VERSION}"
 fi
-prBody="$6"
+prBody="${CHANGELOG}"
 if [ "${prNumber}" ]; then
     echo "> Editing existing PR"
     gh api "repos/${prRepo}/pulls/${prNumber}" --silent --method PATCH -f "title=${prTitle}" -f "body=${prBody}" -f "state=open"
 else
     echo "> Creating PR"
-    gh pr create --repo "${prRepo}" --head "$5:$2" --base "main" --title "${prTitle}" --body "${prBody}"
+    gh pr create --repo "${prRepo}" --head "${GITHUB_REPOSITORY_OWNER}:${INTERNAL_NAME}" --base "main" --title "${prTitle}" --body "${prBody}"
 fi
--- scripts/setupToml.sh
+++ scripts/setupToml.sh
@@ -1,21 +1,21 @@
 echo "> Deleting old"
-rm -rf repo/stable/$1
-rm -rf repo/testing/live/$1
-rm -rf repo/testing/net6/$1
+rm -rf repo/stable/${INTERNAL_NAME}
+rm -rf repo/testing/live/${INTERNAL_NAME}
+rm -rf repo/testing/net6/${INTERNAL_NAME}
 echo "> Making new"
-if [ $2 =~ .*"[TEST]".* ]; then
-    mkdir repo/testing/net6/$1
-    cd repo/testing/net6/$1
+if [ ${MESSAGE} =~ .*"[TEST]".* ]; then
+    mkdir repo/testing/net6/${INTERNAL_NAME}
+    cd repo/testing/net6/${INTERNAL_NAME}
 else
-    mkdir repo/stable/$1
-    cd repo/stable/$1
+    mkdir repo/stable/${INTERNAL_NAME}
+    cd repo/stable/${INTERNAL_NAME}
 fi
 echo "[plugin]" >>manifest.toml
-echo "repository = \"$3.git\"" >>manifest.toml
-echo "owners = [ \"$4\" ]" >>manifest.toml
+echo "repository = \"${URL}.git\"" >>manifest.toml
+echo "owners = [ \"${GITHUB_REPOSITORY_OWNER}\" ]" >>manifest.toml
 echo "project_path = \"\"" >>manifest.toml
-echo "commit = \"$5\"" >>manifest.toml
-echo "changelog = \"$6\"" >>manifest.toml
-echo "version = \"$7\"" >>manifest.toml
+echo "commit = \"${GITHUB_SHA}\"" >>manifest.toml
+echo "changelog = \"${CHANGELOG}\"" >>manifest.toml
+echo "version = \"${VERSION}\"" >>manifest.toml
 echo "> Done"
 cat manifest.toml
