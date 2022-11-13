48548cafedafc1da75316990cd87efba92f7181f
wolfcomp <magnus1997.mho@gmail.com>
2022-11-13 22:34:45 +0100

    refactor: implement Locale and MobData from local file storage

---
+++ DeepDungeonDex/Storage/Locale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DeepDungeonDex.Storage
+{
+    public class Locale : ILoadable<Locale>
+    {
+        public Dictionary<string, string> LocaleDictionary { get; set; } = new();
+
+        public void Save(string path)
+        {
+            StorageHandler.SerializeJsonFile(path, this);
+        }
+
+        [JsonIgnore]
+        public Action<DateTime> Updated { get; set; }
+
+        public Locale Load(string path)
+        {
+            LocaleDictionary = StorageHandler.Deserializer.Deserialize<Dictionary<string, string>>(StorageHandler.ReadFile(path));
+            throw new NotImplementedException();
+        }
+
+        object ILoadable.Load(string path) => Load(path);
+    }
+}
---
+++ DeepDungeonDex/Storage/MobData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace DeepDungeonDex.Storage
+{
+    public class MobData : ILoadable<MobData>
+    {
+        public Dictionary<int, Mob> MobDictionary { get; set; } = new();
+
+        public MobData Load(string path)
+        {
+            var mobs = StorageHandler.Deserializer.Deserialize<Dictionary<string, Mob>>(StorageHandler.ReadFile(path));
+            foreach (var (key, value) in mobs)
+            {
+                var splitInd = key.IndexOf('-');
+                var id = int.Parse(key[..splitInd]);
+                var name = key[(splitInd + 1)..];
+                value.Name = name;
+                MobDictionary.Add(id, value);
+            }
+            return this;
+        }
+
+        public void Save(string path)
+        {
+            MobDictionary.Select(t => (t.Key, t.Value)).ToDictionary(t => $"{t.Key}-{t.Value.Name}", t => t.Value);
+        }
+
+        public Action<DateTime> Updated { get; set; }
+        object ILoadable.Load(string path) => Load(path);
+    }
+
+    public class Mob
+    {
+        [YamlIgnore]
+        public string Name { get; set; }
+        public Weakness Weakness { get; set; }
+        public Aggro Aggro { get; set; }
+        public Threat Threat { get; set; }
+    }
+
+    [Flags]
+    public enum Weakness
+    {
+        None = 0x00,
+        Stun = 0x01,
+        Heavy = 0x02,
+        Slow = 0x04,
+        Sleep = 0x08,
+        Bind = 0x10,
+        Undead = 0x20
+    }
+
+    public enum Aggro
+    {
+        Undefined,
+        Sight,
+        Sound,
+        Proximity,
+        Boss
+    }
+
+    public enum Threat
+    {
+        Undefined,
+        Easy,
+        Caution,
+        Dangerous,
+        Vicious
+    }
+}
