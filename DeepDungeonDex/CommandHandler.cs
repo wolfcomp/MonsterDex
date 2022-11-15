5889746471d7c52f4a0b03023f597f3e79e0fead
wolfcomp <magnus1997.mho@gmail.com>
2022-11-16 00:17:39 +0100

    refactor: add command handler singleton

---
+++ DeepDungeonDex/CommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Dalamud.Game.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepDungeonDex
+{
+    public class CommandHandler
+    {
+        private readonly CommandManager _manager;
+
+        public CommandHandler(CommandManager manager)
+        {
+            _manager = manager;
+        }
+    }
+}
