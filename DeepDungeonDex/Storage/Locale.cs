09690f12f71641c4051ffd9a09c413be498b6a31
wolfcomp <magnus1997.mho@gmail.com>
2022-11-22 15:51:01 +0100

    chore: add full locale data classes

--- DeepDungeonDex/Storage/Locale.cs
+++ DeepDungeonDex/Storage/Locale.cs
@@ -10,22 +10,41 @@ namespace DeepDungeonDex.Storage
 {
     public class Locale : ILoadable<Locale>
     {
-        public Dictionary<string, string> LocaleDictionary { get; set; } = new();
+        public Dictionary<string, string> TranslationDictionary { get; set; } = new();
 
         public void Save(string path)
         {
-            StorageHandler.SerializeJsonFile(path, this);
+            StorageHandler.SerializeJsonFile(path, TranslationDictionary);
         }
-
-        [JsonIgnore]
+        
         public Action<DateTime> Updated { get; set; }
 
         public Locale Load(string path)
         {
-            LocaleDictionary = StorageHandler.Deserializer.Deserialize<Dictionary<string, string>>(StorageHandler.ReadFile(path));
-            throw new NotImplementedException();
+            TranslationDictionary = StorageHandler.Deserializer.Deserialize<Dictionary<string, string>>(StorageHandler.ReadFile(path));
+            return this;
         }
 
+        object ILoadable.Load(string path) => Load(path);
+    }
+
+    public class LocaleKeys : ILoadable<LocaleKeys>
+    {
+        public Dictionary<string, string> LocaleDictionary { get; set; } = new();
+
+        public void Save(string path)
+        {
+            StorageHandler.SerializeJsonFile(path, LocaleDictionary);
+        }
+        
+        public Action<DateTime> Updated { get; set; }
+        
+        public LocaleKeys Load(string path)
+        {
+            LocaleDictionary = StorageHandler.Deserializer.Deserialize<Dictionary<string, string>>(StorageHandler.ReadFile(path));
+            return this;
+        }
+        
         object ILoadable.Load(string path) => Load(path);
     }
 
@@ -33,12 +52,12 @@ namespace DeepDungeonDex.Storage
     {
         public static string? GetLocale(this Locale locale, string key)
         {
-            return locale.LocaleDictionary.TryGetValue(key, out var value) ? value : key;
+            return locale.TranslationDictionary.TryGetValue(key, out var value) ? value : key;
         }
 
         public static string? GetLocale(this IEnumerable<Locale> locales, string key)
         {
-            return locales.FirstOrDefault(l => l.LocaleDictionary.ContainsKey(key))?.LocaleDictionary[key];
+            return locales.FirstOrDefault(l => l.TranslationDictionary.ContainsKey(key))?.TranslationDictionary[key];
         }
     }
 }
