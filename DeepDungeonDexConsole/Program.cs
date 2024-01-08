feb40a762b3d3fde306d9ed21e4d9bae916a7dac
WildWolf <magnus1997.mho@gmail.com>
2024-01-09 00:47:43 +0100

    chore: remove some nuget packages to try and make plogon happy

--- DeepDungeonDexConsole/Program.cs
+++ DeepDungeonDexConsole/Program.cs
@@ -1,10 +1,5 @@
-﻿using System.CommandLine;
-using System.IO.Compression;
-using System.Reflection;
+﻿using System.IO.Compression;
 using System.Text;
-using YamlDotNet.Core.Events;
-using YamlDotNet.Core;
-using YamlDotNet.Serialization;
 
 namespace DeepDungeonDexConsole
 {
@@ -36,75 +31,5 @@ namespace DeepDungeonDexConsole
             archive.Dispose();
             stream.Dispose();
         }
-    }
-
-    public static class Extensions
-    {
-        public static bool TryGetKey(this Dictionary<int, string> dict, string value, out int key)
-        {
-            var k = dict.Where(pair => string.Equals(value, pair.Value, StringComparison.InvariantCultureIgnoreCase)).ToArray();
-            if (k.Any())
-            {
-                key = k.First().Key;
-                return true;
-            }
-
-            key = default;
-            return false;
-        }
-    }
-
-    internal class YamlStringEnumConverter : IYamlTypeConverter
-    {
-        public bool Accepts(Type type) => type.IsEnum;
-
-        public object ReadYaml(IParser parser, Type type)
-        {
-            var items = new List<string>();
-            if (type.GetCustomAttributes<FlagsAttribute>().Any())
-            {
-                parser.TryConsume<SequenceStart>(out _);
-                while (parser.TryConsume<Scalar>(out var scalar))
-                {
-                    items.Add(scalar.Value);
-                }
-                parser.TryConsume<SequenceEnd>(out _);
-            }
-            else if (parser.TryConsume<Scalar>(out var scalar))
-                items.Add(scalar.Value);
-            return Enum.Parse(type, string.Join(", ", items));
-        }
-
-        public void WriteYaml(IEmitter emitter, object? value, Type type)
-        {
-            if (value == null) return;
-            if (type.GetCustomAttributes<FlagsAttribute>().Any())
-            {
-                var str = value.ToString()!.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-                emitter.Emit(new SequenceStart(default, default, false, SequenceStyle.Any));
-                foreach (var s in str)
-                {
-                    emitter.Emit(new Scalar(s));
-                }
-                emitter.Emit(new SequenceEnd());
-            }
-            else
-            {
-                emitter.Emit(new Scalar(value.ToString()!));
-            }
-        }
-    }
-
-    [Flags]
-    public enum Weakness
-    {
-        None = 0x00,
-        Stun = 0x01,
-        Heavy = 0x02,
-        Slow = 0x04,
-        Sleep = 0x08,
-        Bind = 0x10,
-        Undead = 0x20
     }
 }
